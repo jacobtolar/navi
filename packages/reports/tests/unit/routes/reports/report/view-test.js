f071744e24a9362413f5e10551414c4f55a279bf
Matt Graham <mgraham@yahoo-inc.com>
2019-03-15 17:40:01 -0500

    Update: All acceptance tests except navi-report-test are passing on 3.0

--- packages/reports/tests/unit/routes/reports/report/view-test.js
+++ packages/reports/tests/unit/routes/reports/report/view-test.js
@@ -1,133 +1,126 @@
 import EmberObject, { get } from '@ember/object';
 import { run } from '@ember/runloop';
 import { resolve } from 'rsvp';
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('route:reports/report/view', 'Unit | Route | reports/report/view', {
-  needs: [
-    'service:navi-visualizations',
-    'manifest:metric-label',
-    'manifest:table',
-    'manifest:line-chart',
-    'manifest:base',
-    'model:table',
-    'service:bard-facts'
-  ]
-});
+module('Unit | Route | reports/report/view', function(hooks) {
+  setupTest(hooks);
 
-test('model', function(assert) {
-  assert.expect(5);
+  test('model', function(assert) {
+    assert.expect(5);
 
-  const serializedRequest = 'foo',
-    factServiceResponse = 'bar',
-    reportModel = {
-      id: 1,
-      request: {
-        serialize: () => serializedRequest
-      },
-      visualization: {
-        type: 'table',
-        isValidForRequest: () => false, // Test invalid config case
-        rebuildConfig(request, response) {
-          assert.equal(
-            request,
-            reportModel.request,
-            'When config is invalid, rebuildConfig is given raw request object'
-          );
+    const serializedRequest = 'foo',
+      factServiceResponse = 'bar',
+      reportModel = {
+        id: 1,
+        request: {
+          serialize: () => serializedRequest
+        },
+        visualization: {
+          type: 'table',
+          isValidForRequest: () => false, // Test invalid config case
+          rebuildConfig(request, response) {
+            assert.equal(
+              request,
+              reportModel.request,
+              'When config is invalid, rebuildConfig is given raw request object'
+            );
 
-          assert.equal(
-            response,
-            factServiceResponse,
-            'When config is invalid, rebuildConfig is given fact service response'
-          );
+            assert.equal(
+              response,
+              factServiceResponse,
+              'When config is invalid, rebuildConfig is given fact service response'
+            );
+          }
         }
-      }
-    };
+      };
 
-  let route = this.subject({
-    modelFor: () => reportModel,
-    facts: {
-      fetch(request, options) {
-        assert.equal(request, serializedRequest, "Report's serialized request is given to fact service");
+    let route = this.owner.factoryFor('route:reports/report/view').create({
+      modelFor: () => reportModel,
+      facts: {
+        fetch(request, options) {
+          assert.equal(request, serializedRequest, "Report's serialized request is given to fact service");
 
-        assert.deepEqual(
-          options,
-          {
-            page: 1,
-            perPage: 10000,
-            clientId: 'customReports',
-            customHeaders: {
-              uiView: 'report.spv.1'
-            }
-          },
-          'Options from route are passed to fact service'
-        );
+          assert.deepEqual(
+            options,
+            {
+              page: 1,
+              perPage: 10000,
+              clientId: 'customReports',
+              customHeaders: {
+                uiView: 'report.spv.1'
+              }
+            },
+            'Options from route are passed to fact service'
+          );
 
-        return resolve({ response: factServiceResponse });
-      }
-    }
-  });
-
-  run(() => route.model()).then(model => {
-    assert.equal(
-      model,
-      factServiceResponse,
-      'Model hook returns response from fact service wrapped in a PromiseObject'
-    );
-  });
-});
-
-test('invalid visualization', function(assert) {
-  assert.expect(1);
-
-  this.register(
-    'manifest:invalid-type',
-    EmberObject.extend({
-      typeIsValid: () => false
-    })
-  );
-
-  let route = this.subject(),
-    report = {
-      visualization: {
-        type: 'invalid-type'
-      }
-    };
-
-  run(() => {
-    route._setValidVisualizationType(null, report);
-  });
-
-  assert.equal(get(report, 'visualization.type'), 'table', 'Any invalid visualization types are defaulted to table');
-});
-
-test('runReport action', function(assert) {
-  assert.expect(2);
-
-  let request = { serialize: () => 'foo' },
-    parentModel = {
-      request
-    },
-    route = this.subject({
-      parentModel,
-      previousRequest: 'foo',
-      refresh() {
-        throw new Error('The route should not refresh if the request has not changed');
+          return resolve({ response: factServiceResponse });
+        }
       }
     });
 
-  /* == Request has no changes == */
-  route.send('runReport');
+    run(() => route.model()).then(model => {
+      assert.equal(
+        model,
+        factServiceResponse,
+        'Model hook returns response from fact service wrapped in a PromiseObject'
+      );
+    });
+  });
 
-  /* == Request has been changed == */
-  route.refresh = () => {
-    route.previousRequest = request.serialize();
-    assert.ok(true, 'Action asks route to refresh model');
-  };
-  request.serialize = () => 'bar';
-  route.send('runReport');
+  test('invalid visualization', function(assert) {
+    assert.expect(1);
 
-  /* == Request has been changed again== */
-  request.serialize = () => 'foo';
-  route.send('runReport');
+    this.owner.register(
+      'manifest:invalid-type',
+      EmberObject.extend({
+        typeIsValid: () => false
+      })
+    );
+
+    let route = this.owner.lookup('route:reports/report/view'),
+      report = {
+        visualization: {
+          type: 'invalid-type'
+        }
+      };
+
+    run(() => {
+      route._setValidVisualizationType(null, report);
+    });
+
+    assert.equal(get(report, 'visualization.type'), 'table', 'Any invalid visualization types are defaulted to table');
+  });
+
+  test('runReport action', function(assert) {
+    assert.expect(2);
+
+    let request = { serialize: () => 'foo' },
+      parentModel = {
+        request
+      },
+      route = this.owner.factoryFor('route:reports/report/view').create({
+        parentModel,
+        previousRequest: 'foo',
+        refresh() {
+          throw new Error('The route should not refresh if the request has not changed');
+        }
+      });
+
+    /* == Request has no changes == */
+    route.send('runReport');
+
+    /* == Request has been changed == */
+    route.refresh = () => {
+      route.previousRequest = request.serialize();
+      assert.ok(true, 'Action asks route to refresh model');
+    };
+    request.serialize = () => 'bar';
+    route.send('runReport');
+
+    /* == Request has been changed again== */
+    request.serialize = () => 'foo';
+    route.send('runReport');
+  });
 });
